3ab79a534bafc31714717249c8b5e6f932c87274
j-hha <j-hha@bla.bla>
2017-06-01 15:32:56 -0400

    preparing for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,18 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://coffeecompass.herokuapp.com' #, '*'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-
-  allow do
     origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
