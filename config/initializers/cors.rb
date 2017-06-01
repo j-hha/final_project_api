ee4dbdc7be65f12e594abd4f770cb76e735c49f6
j-hha <j-hha@bla.bla>
2017-06-01 15:07:47 -0400

    trying to fix issue with login on mobile

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,18 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://coffeecompass.herokuapp.com'
+    origins 'https://coffeecompass.herokuapp.com' #, '*'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :options, :head]
   end
 end
